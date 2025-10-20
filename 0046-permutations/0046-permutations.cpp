13a235ef04de06249643d3619b22961fb6fadc71
ZJIAYIN <150604517+ZJIAYIN@users.noreply.github.com>
2025-10-21 00:13:59 +0800

    Time: 4 ms (12.66%), Space: 12.9 MB (6.52%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -8,32 +8,31 @@ public:
 
         int len = nums.size();
 
-        vector<int> vis(len);
+        vector<int>vis(len);
 
-        int step = 0;
-
-        solve(nums,len,vis,ans,comb,step);
+        solve(ans,comb,nums,vis,len);
 
         return ans;
 
     }
 
-    void solve(vector<int>& nums,int len,vector<int>vis,vector<vector<int>>& ans,vector<int>comb,int step){
+    void solve(vector<vector<int>>&ans,vector<int>comb,vector<int>& nums,vector<int>vis,int len){
         
-        if(step >= len){
+        if(comb.size() == len){
             ans.push_back(comb);
             return;
         };
 
         for(int i = 0;i<len;i++){
-            if(vis[i] == 0){
-                comb.push_back(nums[i]);
+            if(!vis[i]){
                 vis[i] = 1;
-                solve(nums,len,vis,ans,comb,step+1);
-                comb.pop_back();
+                comb.push_back(nums[i]);
+                solve(ans,comb,nums,vis,len);
                 vis[i] = 0;
+                comb.pop_back();
             };
         };
 
     };
+
 };
